4d681f8b41adf6dac23465bf110cf5f2063f2dde
Eleftherios Moschos <tmoschos@bigpond.com>
2020-06-16 23:53:07 +0930

    Setting up Repo with HTML file structure created and some JS

---
+++ Homework-5/Develop/script.js
@@ -0,0 +1,20 @@
+//Display Day and Time in Heading
+$("#currentDay").html(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+//Change display of tasks with reference to time of day
+var hour = document.getElementById("hour");
+console.log(currentDay.innerHTML)
+//$("currentDay").text = moment().format("MMM Do YYYY");
+//var currentHour = document.getElementById("test");
+//currentHour.innerHTML = moment().format('H');
+var hourObject = moment().format('H');
+var correctHour = new Date().getHours();
+var hourString = JSON.stringify(hourObject);
+var hourInteger = parseInt(hourString);
+console.log(parseInt(hourObject)===21);
+console.log(correctHour)
+console.log(typeof(hourString));
+console.log(hourString);
+console.log(typeof(hourInteger));
+console.log(hourInteger==="21");
+//if (hourInteger==9) {
